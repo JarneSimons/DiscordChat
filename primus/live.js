bed4afab4069627c1b115f5842fc865a93ab2523
JarneSimons <r0901961@student.thomasmore.be>
2023-11-24 12:19:53 +0100

    feat: add discord

---
+++ primus/live.js
@@ -0,0 +1,19 @@
+module.exports.go = (server) => {
+    const Primus = require('primus');
+    const primus = new Primus(server, {/* options */});
+
+    // check if conncection, then console.log
+    primus.on('connection', (spark) => {
+        console.log('connection😃😃');
+
+        // check if data, then console.log
+        spark.on('data', (data) => {
+            console.log('data😁😁', data);
+
+            //send data back to all clients
+            primus.write(data); // all
+            
+        });
+    });
+}
+
